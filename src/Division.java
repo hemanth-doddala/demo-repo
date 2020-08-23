975b4c9d969c3e5b6a0024908444ecf5a4262656
hemanth-doddala <dhssr@hotmail.co.in>
2020-08-23 22:57:58 +0530

    division

---
+++ src/Division.java
@@ -0,0 +1,11 @@
+
+public class Division {
+	public static int divide(int a, int b) {
+		return a/b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(divide(4,2));
+	}
+
+}
